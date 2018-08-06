d67999dd0bf13d82e68c350d74a14debdd8bac6d
Myself <sonichauhan83@gmail.com>
2018-08-06 12:48:08 +0530

    First commited to newly created repo from xcode

---
+++ mapDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mapDemo
+//
+//  Created by Rajesh Vishnani on 06/08/18.
+//  Copyright © 2018 Rajesh Vishnani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
